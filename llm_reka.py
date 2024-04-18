8a7c539987073cd27dc98f5d1c28381b71f682df
Simon Willison <swillison@gmail.com>
2024-04-17 19:17:22 -0700

    WIP streaming implementation, refs #2

--- llm_reka.py
+++ llm_reka.py
@@ -1,6 +1,7 @@
-import click
 import llm
-import reka
+import httpx
+from httpx_sse import connect_sse
+import json
 
 
 MODELS = ("reka-core", "reka-edge", "reka-flash")
@@ -16,17 +17,44 @@ def register_models(register):
 
 class Reka(llm.Model):
     needs_key = "reka"
+    can_stream = True
 
     def __init__(self, model_id):
         self.model_id = model_id
 
     def execute(self, prompt, stream, response, conversation):
-        reka.API_KEY = llm.get_key("", "reka", "LLM_REKA_KEY")
-        response = reka.chat(
-            human=prompt.prompt,
-            # conversation_history=conversation_history,
-        )
-        yield response["text"]
-
-    def __str__(self):
-        return "Reka: {}".format(self.model_id)
+        with httpx.Client() as client:
+            with connect_sse(
+                client,
+                "POST",
+                "https://api.reka.ai/chat",
+                headers={
+                    "x-api-key": llm.get_key("", "reka", "LLM_REKA_KEY"),
+                },
+                json={
+                    "conversation_history": [
+                        {
+                            "type": "human",
+                            "text": prompt.prompt,
+                        }
+                    ],
+                    "use_search_engine": False,
+                    "use_code_interpreter": False,
+                    "model_name": self.model_id,
+                    "stream": True,
+                    # "random_seed": 1713403859366
+                },
+            ) as event_source:
+                last_text = ""
+                accumulated = []
+                for sse in event_source.iter_sse():
+                    accumulated.append(sse.json())
+                    info = json.loads(sse.data)
+                    text = info["text"]
+                    if text != last_text:
+                        # Figure out what's new and yield that
+                        new_text = text[len(last_text) :]
+                        yield new_text
+                        last_text = text
+                print()
+                print(json.dumps(accumulated, indent=2))
